ec535a346caa37f34da24009e69434e2d2da5682
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-24 09:24:34 +0200

    created function to return true / false

--- js/script.js
+++ js/script.js
@@ -1,79 +1,34 @@
-
-
-
-// while (generatedNumber.length < 5) {
-    //     num = randomNumber (1 ,100);
-    //     if (generatedNumber.includes(num) == 0) {
-        //         generatedNumber.push(num);
-        
-        //     }
-        //     console.log(generatedNumber)
-        // }
-        
-        // const pageEL = document.querySelector('ul');
-        // const visualEl = document.querySelector('h1');
-        
-        // for (let index = 0; index < generatedNumber.length; index++) {
-            //     const pEl = document.createElement('li');
-            //     pEl.innerHTML = generatedNumber[index];
-            //     pageEL.appendChild(pEl);
-            // }
-            
-            
-            // setTimeout(function () {
-                //     pageEL.innerHTML ='';
-                //     visualEl.innerHTML= '';
-                
-                //     setTimeout(function() {
-                    //         let addedNum =[];
-                    
-                    //         for (let index = 0; index < generatedNumber.length; index++) {
-                        //             let userNumber = Number.parseInt(promt('write the numbers you remember'));
-                        //             if(generatedNumber.includes(userNumber) && (addedNum.includes(userNumber)==0)){
-                            //                 addedNum.push(userNumber);
-                            //             }
-                            //         }
-                            
-                            //         let resoult = 'nice you find out numbers';
-                            
-                            //         if (addedNum.length == 0) {
-                                //             resoult ='Too bad you didn t remember any numbers';
-                                //             for (let index = 0; index < generatedNumber.length; index++) {
-                                    //                 resoult+= ' ' + generatedNumber[index];
-                                    //                 if(index != generatedNumber.length - 1)
-                                        //                     resoult +=' ';
-                                    
-                                    //             }
-                                    //         }
-                                    //         for (let index = 0; index < addedNum.length; index++) {
-                                        //             resoult+= ' ' + addedNum[index];
-                                        //             }if(index != addedNum.length - 1){
-//                 resoult +=' ';
-//         }
-
-//         visualEl.append(resoult);
-//     }, 1);
-// }, 30000,)
-
-
-const generatedNumber = [];
 const containerEl = document.querySelector('#container');
 
+const generatedNumber = [];
 for (let index = 0; index < 5; index++) {
     generatedNumber.push(getUniqueRandomInt(generatedNumber, 0, 100))
     // console.log(generatedNumber);
-    const newNumberEl = document.createElement('article');
-    newNumberEl.append(generatedNumber[index]);
-    containerEl.appendChild(newNumberEl);
+    
 }
+
+prindedNumber(containerEl, generatedNumber);
+
+function prindedNumber(container, numGenerated) {
+    for (let index = 0; index < numGenerated.length; index++){
+        const newNumberEl = document.createElement('article');
+        newNumberEl.append(numGenerated[index]);
+        containerEl.appendChild(newNumberEl);
+    };
+
+    }
 
 setTimeout(function(){
     containerEl.innerHTML = '';
-    setTimeout(function(){
-        console.log(retrieveGuesses (5));
-
-    }, 0);
+    let requestedNumb = [];
     
+    setTimeout(function(){
+        requestedNumb = retrieveGuesses (5);
+        console.log(generatedNumber, requestedNumb);
+        prindedNumber(containerEl, generatedNumber);
+        prindedNumber(containerEl, rightAnswers(generatedNumber, requestedNumb));
+    }, 0);
+    console.log(requestedNumb);
 }, 30000);
 
 function retrieveGuesses(numberOfGuesses) {
@@ -85,6 +40,17 @@ function retrieveGuesses(numberOfGuesses) {
     return requestedNumb;
 }
 
+function rightAnswers(numbers, guesses) {
+    const answers = [];
+    for (let index = 0; index < numbers.length; index++) {
+        if(guesses.includes(numbers[index]) === true ){
+            answers.push(true);
+        }else{
+            answers.push(false);
+        }
+    }
+    return answers;
+}
 
 
 
